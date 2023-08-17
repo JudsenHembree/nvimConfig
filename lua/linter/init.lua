ad095030c8f9a277c73de339eb9ac607089d861b
Judsen <jhembre@g.clemson.edu>
2023-08-17 04:21:36 +0000

    working docker pullable

--- lua/linter/init.lua
+++ lua/linter/init.lua
@@ -2,9 +2,62 @@ local linter = {}
 
 function linter.setup()
     local ft = require('guard.filetype')
+    local diag_fmt = require('guard.lint').diag_fmt
+    -- run it through docker instead???
+    ft('cpp'):fmt(
+    {
+        cmd = "clang-format",
+        stdin = true
+    })
+    :lint(
+    {
+        cmd = "docker",
+        args = 
+        {
+            "run",
+            "-v",
+            "$PWD:/tmp",
+            "jhembre/clang_setup:latest",
+            "clang-tidy",
+            "--quiet"
+        },
+        stdin = false,
+        output_fmt = function(result, buf)
+            local map = {
+                'error',
+                'warning',
+                'information',
+                'hint',
+                'note',
+            }
 
-    ft('cpp'):fmt('clang-format')
-       :lint('clang-tidy')
+            local messages = vim.split(result, '\n')
+            local diags = {}
+            vim.tbl_map(function(mes)
+                local message
+                local severity
+                for idx, t in ipairs(map) do
+                    local _, p = mes:find(t)
+                    if p then
+                        message = mes:sub(p + 2, #mes)
+                        severity = idx
+                        local pos = mes:match([[(%d+:%d+)]])
+                        local lnum, col = unpack(vim.split(pos, ':'))
+                        diags[#diags + 1] = diag_fmt(
+                        buf,
+                        tonumber(lnum) - 1,
+                        tonumber(col),
+                        message,
+                        severity > 4 and 4 or severity,
+                        'clang-tidy'
+                        )
+                    end
+                end
+            end, messages)
+
+            return diags
+        end,
+    })
 
     -- Call setup() LAST!
     require('guard').setup({
@@ -16,3 +69,4 @@ function linter.setup()
 end
 
 return linter
+
